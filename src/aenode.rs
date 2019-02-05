11ec5bf1ec2a777ca7d17ea7a6d908c73505c3de
John Newby <john@newby.org>
2019-02-05 15:59:38 +0100

    Calculate Prologue!

--- src/aenode.rs
+++ src/aenode.rs
@@ -1,5 +1,13 @@
+use base58check::FromBase58Check;
+use byteorder::{BigEndian, WriteBytesExt};
+use curl::easy::Easy;
 use regex::Regex;
+use serde_json::Value;
+use simple_error::SimpleError;
+use std::fmt::{Error, Write};
 use std::net::IpAddr;
+
+type RlpError = Box<std::error::Error>;
 
 pub struct Aenode {
     pub pub_key: String,
@@ -17,6 +25,56 @@ impl Aenode {
             port: captures[3].parse()?,
         })
     }
+
+    pub fn prologue(&self, port: u16) -> Result<Vec<u8>, RlpError>
+    {
+        let mut easy = Easy::new();
+        let url = format!("http://{}:{}/v2/status", self.address, port);
+        println!("{}", url);
+        easy.url(&url)?;
+        let mut v = Vec::<u8>::new();
+        {
+            let mut transfer = easy.transfer();
+            transfer.write_function(|data| {
+                v.extend_from_slice(data);
+                Ok(data.len())
+            });
+            transfer.perform();
+        }
+        let json: Value = serde_json::from_str(&String::from_utf8(v)?)?;
+        println!("{:?}", json);
+        let gen_hash = match json["genesis_key_block_hash"].as_str() {
+            Some(x) => String::from(x),
+            None => bail!("Genesis block not found"),
+        };
+        let gen_hash = String::from_utf8(gen_hash[3..].as_bytes().to_vec())?;
+        let network_id = match json["network_id"].as_str() {
+            Some(x) => String::from(x),
+            None => bail!("Network id not found"),
+        };
+        prologue(1, &gen_hash, &network_id)
+    }
+}
+
+/*
+ * decode base 58, adding the version byte onto the returned value
+ */
+fn decodebase58check(data: &String) -> Vec<u8> {
+    let mut result = base58check::FromBase58Check::from_base58check(data.as_str()).unwrap().1;
+    result.insert(0, base58check::FromBase58Check::from_base58check(data.as_str()).unwrap().0);
+    result
 }
 
 
+
+pub fn prologue(version: u64, genesis_hash: &String, network_id: &String) ->
+    Result<Vec<u8>, RlpError>
+{
+    let mut genesis_binary = decodebase58check(&genesis_hash);
+    let mut network_id_binary = network_id.as_bytes();
+    let mut result = vec!();
+    result.write_u64::<BigEndian>(version);
+    result.append(&mut genesis_binary);
+    result.append(&mut network_id_binary.to_vec());
+    Ok(result)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 #[macro_use] extern crate arrayref;
+extern crate base58check;
 extern crate byteorder;
+extern crate curl;
 #[macro_use]extern crate lazy_static;
 extern crate regex;
 extern crate rlp;
-extern crate base58check;
-extern crate snow;
-extern crate hex;
-#[macro_use]extern crate serde_rlp;
 #[macro_use]extern crate serde_derive;
 #[macro_use]extern crate serde_json;
+#[macro_use]extern crate serde_rlp;
+#[macro_use]extern crate simple_error;
+extern crate snow;
+extern crate hex;
 
 use base58check::{FromBase58Check};
 use byteorder::{ByteOrder, BigEndian, ReadBytesExt};
@@ -39,11 +41,11 @@ fn foo() {
 
 pub fn main() {
     let mut buf = vec![0u8; 65535];
-    let prologue: [u8;50] = [0,0,0,0,0,0,0,1,108,21,218,110,191,175,2,120,254,175,77,241,176,241,169,130,85,7,174,123,154,73,75,195,76,145,113,63,56,221,87,131,97,101,95,109,97,105,110,110,101,116];
     let args: Vec<String> = std::env::args().collect();
     let node = aenode::Aenode::new(&args[1])
         //&String::from("aenode://pp_2kzKvxEg9NbBXn6krSeNec8kSeiJy8GXxnoTanX2zr1ffABvqd@192.168.111.81:3015"))
         .unwrap();
+    let prologue = node.prologue(3013).unwrap();
     let mut gen_hash = "pbtwgLrNu23k9PA6XCZnUbtsvEFeQGgavY4FS2do3QP8kcp2z".from_base58check().unwrap().1;
     gen_hash.insert(0, "pbtwgLrNu23k9PA6XCZnUbtsvEFeQGgavY4FS2do3QP8kcp2z".from_base58check().unwrap().0);
 
